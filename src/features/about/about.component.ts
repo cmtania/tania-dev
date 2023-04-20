876b49498be5c8d872907d2ea10f3d4886cfa0b4
Christian M. Tania <taniachristian01@gmail.com>
2023-04-20 11:29:25 +0800

    Initial Mobile responsive
    Initial Mobile responsive

--- src/features/about/about.component.ts
+++ src/features/about/about.component.ts
@@ -7,7 +7,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  @Input() isCertClick: boolean = false;
+  @Input() isAboutClick: boolean = false;
   certificateLink: Array<string> = ['https://courses.buri.io/view/user/certificate/c725f9b0-5bed-4951-a06a-fe0b7bec730a/pdf',
   'https://courses.buri.io/view/user/certificate/10be5a5d-8d80-459d-a353-190e255f54ce/pdf'];
   
